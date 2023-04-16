82e14ab47310812933d4f6826a5538ba27a04b72
alexiabega <alexiabega0930@gmail.com>
2023-04-16 11:15:31 +0200

    partial fix

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,3 @@
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
 output "rds_endpoint" {
   value = module.database.db-instance
 }
@@ -12,4 +8,20 @@ output "db-port" {
 
 output "db-address" {
   value = module.database.db-address
+}
+
+output "bucket_name" {
+  value = module.storage.web-client-bucket-name
+}
+
+output "ecr_repository_name" {
+  value = module.cluster.ecr_repository_name
+}
+
+output "db_username" {
+  value = var.DB_USERNAME
+}
+
+output "db_password" {
+  value = var.DB_PASSWORD
 }
